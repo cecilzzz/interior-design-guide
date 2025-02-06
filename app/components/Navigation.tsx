f1d57e7573db5bafe2a2c00ac11defbbaba88f58
cecilzzz <leviazmd@gmail.com>
2025-02-06 23:56:48 +0800

    update

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -49,7 +49,7 @@ const navItems: NavItem[] = [
 export default function Navigation() {
   return (
     <header className="fixed top-0 left-0 right-0 bg-[#424144] z-50">
-      <div className="max-w-[120rem] mx-auto px-12 lg:px-24">
+      <div className="max-w-[120rem] mx-auto px-12">
         <div className="flex items-center justify-between h-[100px]">
           {/* Logo container */}
           <Link 
