fdfe9d753ed6dc074140a4eb70e4eddd1a5b0e96
cecilzzz <leviazmd@gmail.com>
2025-03-06 14:17:18 +0800

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { getAllPosts } from '@/app/lib/posts';
+import { getAllPosts } from '@/app/lib/markdownProcessor';
 import { getImageUrl } from '@/app/lib/imageUtils';
 import Sidebar from '@/app/components/Sidebar';
 import type { Metadata } from 'next';
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,4 @@
-import { getAllPosts } from '@/app/lib/posts';
+import { getAllPosts } from '@/app/lib/markdownProcessor';
 import { getImageUrl } from '@/app/lib/imageUtils';
 import PostGrid from '@/app/components/PostGrid';
 
