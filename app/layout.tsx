a1e2d833de00307133e0cc35422525cd3b5734fd
cecilzzz <leviazmd@gmail.com>
2025-06-10 13:04:19 +0800

    Bing驗證

--- app/layout.tsx
+++ app/layout.tsx
@@ -190,18 +190,22 @@ export default function RootLayout({
           strategy="afterInteractive"
         >
           {`
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            const isOwner = document.cookie.includes('site_owner=true');
+            
+            if (!isOwner) {
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-            ym(102547338, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                webvisor:true
-            });
+              ym(102547338, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+              });
+            }
           `}
         </Script>
         <noscript>
@@ -213,6 +217,22 @@ export default function RootLayout({
             />
           </div>
         </noscript>
+        <Script 
+          id="microsoft-clarity" 
+          strategy="afterInteractive"
+        >
+          {`
+            const isOwner = document.cookie.includes('site_owner=true');
+            
+            if (!isOwner) {
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/rx56dgri6x";
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", 0);
+            }
+          `}
+        </Script>
         <meta name="p:domain_verify" content="b23e59bbecfac7fc71535e2c969afc73" />
         <meta name="yandex-verification" content="ca5ba5cc7c68f9d6" />
         <link rel="icon" href="/favicon.png" type="image/png" />
