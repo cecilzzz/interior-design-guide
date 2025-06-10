b3ba7ce319f4c8dd0b569db48b1ac2ef9d02c91f
cecilzzz <leviazmd@gmail.com>
2025-06-10 12:24:11 +0800

    Yandex驗證

--- app/layout.tsx
+++ app/layout.tsx
@@ -184,7 +184,37 @@ export default function RootLayout({
             }
           `}
         </Script>
+        {/* Yandex Metrica */}
+        <Script 
+          id="yandex-metrica"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(102547338, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+            });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img 
+              src="https://mc.yandex.ru/watch/102547338" 
+              style={{position: 'absolute', left: '-9999px'}} 
+              alt="Yandex Metrica" 
+            />
+          </div>
+        </noscript>
         <meta name="p:domain_verify" content="b23e59bbecfac7fc71535e2c969afc73" />
+        <meta name="yandex-verification" content="ca5ba5cc7c68f9d6" />
         <link rel="icon" href="/favicon.png" type="image/png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
